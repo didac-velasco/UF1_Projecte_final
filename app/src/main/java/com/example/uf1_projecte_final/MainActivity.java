fedae1c1799409f281fdd808aab38ab6a4633809
didac-velasco <Didac2003velasco>
2023-01-17 16:26:46 +0100

    PROVA CAMBI DE PAG

--- app/src/main/java/com/example/uf1_projecte_final/MainActivity.java
+++ app/src/main/java/com/example/uf1_projecte_final/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.uf1_projecte_final;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button iniciJoc = (Button) findViewById(R.id.btn_inici_joc);
+
+        iniciJoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( MainActivity.this, Preguntes.class); //crear intent
+                startActivity(intent); //executar intent
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/uf1_projecte_final/Preguntes.java
@@ -0,0 +1,30 @@
+package com.example.uf1_projecte_final;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class Preguntes extends AppCompatActivity {
+    TextView pregunta, finalbuenas;
+    ImageButton btnimageButton1, btnimageButton2, btnimageButton3, btnimageButton4, btnimageButton5, btnimageButton6;
+    Button btnsiguiente, btnanterior;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.joc_p1);
+        pregunta = findViewById(R.id.textViewPregunta);
+        btnimageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        btnimageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        btnimageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+        btnimageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        btnimageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        btnimageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+
+        btnsiguiente = findViewById(R.id.seguent);
+        btnanterior = findViewById(R.id.anterior);
+
+        //pregunta.setText("Busqueu aquest carreu i situeu-lo al plànol");
+    }
+}
