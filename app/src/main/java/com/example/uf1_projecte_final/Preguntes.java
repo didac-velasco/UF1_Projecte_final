04463281720bb2bdd05bdac287d3ca2468f470d9
Arnau Roig Boada <aroig3@institutjaumehuguet.cat>
2023-01-24 17:08:50 +0100

    format de preguntes

--- app/src/main/java/com/example/uf1_projecte_final/Preguntes.java
+++ app/src/main/java/com/example/uf1_projecte_final/Preguntes.java
@@ -1,5 +1,6 @@
 package com.example.uf1_projecte_final;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class Preguntes extends AppCompatActivity {
-    int contadorPreguntes=0, errorsInt=0, encertsInt=0;
+    int contadorPreguntes=1, errorsInt=0, encertsInt=0, idPregunta=0;
 
     private ArrayList<String> preguntesArray = new ArrayList();
 
@@ -28,43 +29,142 @@ public class Preguntes extends AppCompatActivity {
         ImageButton btnimageButton6 = (ImageButton) findViewById(R.id.imageButton6);
         //ImageView imatge = (ImageView) findViewById(R.id.imageView2);
         EditText encerts = (EditText) findViewById(R.id.idEncerts);
+        EditText errors = (EditText) findViewById(R.id.idErrors);
 
 
-        Button btnsiguiente = findViewById(R.id.seguent);
-        Button btnanterior = findViewById(R.id.btAnterior);
+        ImageButton btnanterior = (ImageButton) findViewById(R.id.btAnterior);
 
         //afegim preguntes a la llista
         preguntesArray.add("BUSQUEU AQUEST CARREU I SITUEU-LO AL PLÀNOL. (CAL RESPONDRE PER AVANÇAR");
         preguntesArray.add("Durant la visita haureu d’anar-vos fixant si hi ha recordatoris referents a la ciutat tal com hem vist a fora. Quantifiqueu-los i al final del recorregut trieu la resposta correcta.");
+        preguntesArray.add("aaa");
+        preguntesArray.add("bbb");
+        preguntesArray.add("ccc");
+        preguntesArray.add("ddd");
 
-        //carreguem Array
-        //Preguntes preg = new Preguntes(pregunta, imatge, resposta);
-        //preguntesArray.add(preg);
+        preguntaa.setText(preguntesArray.get(0));
 
-        preguntaa.setText(preguntesArray.get(contadorPreguntes));
-
-        btnsiguiente.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                contadorPreguntes++;
-                preguntaa.setText(preguntesArray.get(contadorPreguntes));
-               // imatge.setBackgroundResource(R.drawable.aaaaa);
-
-            }
-        });
         btnanterior.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 contadorPreguntes--;
-                preguntaa.setText(preguntesArray.get(contadorPreguntes));
+                idPregunta--;
+                preguntaa.setText(preguntesArray.get(idPregunta));
             }
         });
         btnimageButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                encertsInt++;
-                String num= encertsInt+""; //passem a String
-                encerts.setText(num);
+                if(contadorPreguntes==1){
+                    idPregunta++;
+                    preguntaa.setText(preguntesArray.get(idPregunta));
+                    contadorPreguntes++;
+
+
+                    encertsInt++;
+                    String num= encertsInt+""; //passem a String
+                    encerts.setText(num);
+                } else {
+                    errorsInt++;
+                    String num= errorsInt+""; //passem a String
+                    errors.setText(num);
+                }
+            }
+        });
+        btnimageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorPreguntes==2){
+                    idPregunta++;
+                    preguntaa.setText(preguntesArray.get(idPregunta));
+                    contadorPreguntes++;
+
+
+                    encertsInt++;
+                    String num= encertsInt+""; //passem a String
+                    encerts.setText(num);
+                } else {
+                    errorsInt++;
+                    String num= errorsInt+""; //passem a String
+                    errors.setText(num);
+                }
+            }
+        });
+        btnimageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorPreguntes==3){
+                    idPregunta++;
+                    preguntaa.setText(preguntesArray.get(idPregunta));
+                    contadorPreguntes++;
+
+
+                    encertsInt++;
+                    String num= encertsInt+""; //passem a String
+                    encerts.setText(num);
+                } else {
+                    errorsInt++;
+                    String num= errorsInt+""; //passem a String
+                    errors.setText(num);
+                }
+            }
+        });
+        btnimageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorPreguntes==4){
+                    idPregunta++;
+                    preguntaa.setText(preguntesArray.get(idPregunta));
+                    contadorPreguntes++;
+
+
+                    encertsInt++;
+                    String num= encertsInt+""; //passem a String
+                    encerts.setText(num);
+                } else {
+                    errorsInt++;
+                    String num= errorsInt+""; //passem a String
+                    errors.setText(num);
+                }
+            }
+        });
+        btnimageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorPreguntes==5){
+                    idPregunta++;
+                    preguntaa.setText(preguntesArray.get(idPregunta));
+                    contadorPreguntes++;
+
+
+                    encertsInt++;
+                    String num= encertsInt+""; //passem a String
+                    encerts.setText(num);
+                } else {
+                    errorsInt++;
+                    String num= errorsInt+""; //passem a String
+                    errors.setText(num);
+                }
+            }
+        });
+        btnimageButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(contadorPreguntes==6){
+                    idPregunta++;
+                    contadorPreguntes++;
+
+                    encertsInt++;
+                    String num= encertsInt+""; //passem a String
+                    encerts.setText(num);
+
+                    Intent intent = new Intent( Preguntes.this, MainActivity.class); //crear intent
+                    startActivity(intent); //executar intent
+                } else {
+                    errorsInt++;
+                    String num= errorsInt+""; //passem a String
+                    errors.setText(num);
+                }
             }
         });
     }
