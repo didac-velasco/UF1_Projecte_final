5cddcf966f91eafdbf14c1d21588a8fb9dd3b181
Arnau Roig Boada <aroig3@institutjaumehuguet.cat>
2023-01-25 20:44:17 +0100

    historia

--- app/src/main/java/com/example/uf1_projecte_final/Historia.java
+++ app/src/main/java/com/example/uf1_projecte_final/Historia.java
@@ -1,11 +1,25 @@
 package com.example.uf1_projecte_final;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Historia extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.historia);
+
+        ImageButton btnanterior = (ImageButton) findViewById(R.id.btAnterior);
+
+        btnanterior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( Historia.this, MainActivity.class); //crear intent
+                startActivity(intent); //executar intent
+            }
+        });
     }
 }
