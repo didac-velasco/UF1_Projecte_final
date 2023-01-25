7b6618581fba270692101e6098d44718161a6454
Arnau Roig Boada <aroig3@institutjaumehuguet.cat>
2023-01-25 19:39:28 +0100

    preguntesEncertsErrors

--- app/src/main/java/com/example/uf1_projecte_final/PreguntesJoc1.java
+++ app/src/main/java/com/example/uf1_projecte_final/PreguntesJoc1.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,7 +22,7 @@ public class PreguntesJoc1 extends AppCompatActivity {
         setContentView(R.layout.joc1);
 
         //recuperem valors
-        TextView preguntaa = findViewById(R.id.textViewPregunta);
+        TextView omplirPregunta = findViewById(R.id.textViewPregunta);
         ImageView imatgee = findViewById(R.id.imageView2);
 
         ImageButton btnimageButton1 = (ImageButton) findViewById(R.id.imageButton1);
@@ -38,34 +39,38 @@ public class PreguntesJoc1 extends AppCompatActivity {
 
         //afegim preguntes a la llista
         preguntesArray.add("BUSQUEU AQUEST CARREU I SITUEU-LO AL PLÀNOL. (CAL RESPONDRE PER AVANÇAR");
-        preguntesArray.add("Segons la imatge, on creieu que estava situat l’orgue anterior? Situeu-lo al plànol");
-        preguntesArray.add("Observeu bé la imatge i responeu.");
+        preguntesArray.add("SEGONS LA IMATGE, ON CREIEU QUE ESTAVA SITUAT L'ORGUE ANTERIOR? SITUEU-LO AL PLÀNOL");
 
-        preguntaa.setText(preguntesArray.get(0));
+        omplirPregunta.setText(preguntesArray.get(0));
 
         btnanterior.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                contadorPreguntes--;
-                idPregunta--;
-                preguntaa.setText(preguntesArray.get(idPregunta));
+                if (idPregunta>0){
+                    contadorPreguntes--;
+                    idPregunta--;
+                    encertsInt--;
+                    omplirPregunta.setText(preguntesArray.get(idPregunta));
+                } else {
+                    encertsInt=0;
+                    Toast.makeText(getApplicationContext(), "No hi han més preguntes!", Toast.LENGTH_SHORT).show();
+                }
+                encerts.setText(ErrorsEncerts(encertsInt));
             }
         });
         btnimageButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(contadorPreguntes==1){
-                    Intent intent = new Intent( PreguntesJoc1.this, PreguntesJoc2.class); //crear intent
-                    startActivity(intent); //executar intent
-
-
                     encertsInt++;
-                    String num= encertsInt+""; //passem a String
-                    encerts.setText(num);
+                    encerts.setText(ErrorsEncerts(encertsInt));
+
+                    idPregunta++;
+                    omplirPregunta.setText(preguntesArray.get(idPregunta));
+                    contadorPreguntes++;
                 } else {
                     errorsInt++;
-                    String num= errorsInt+""; //passem a String
-                    errors.setText(num);
+                    errors.setText(ErrorsEncerts(errorsInt));
                 }
             }
         });
@@ -73,97 +78,50 @@ public class PreguntesJoc1 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(contadorPreguntes==2){
-                    idPregunta++;
-                    preguntaa.setText(preguntesArray.get(idPregunta));
-                    contadorPreguntes++;
-
-
                     encertsInt++;
-                    String num= encertsInt+""; //passem a String
-                    encerts.setText(num);
+                    encerts.setText(ErrorsEncerts(encertsInt));
+
+                    Intent intent = new Intent( PreguntesJoc1.this, PreguntesJoc2.class); //crear intent
+                    intent.putExtra("errors", errorsInt);
+                    intent.putExtra("encerts", encertsInt);
+                    startActivity(intent); //executar intent
                 } else {
                     errorsInt++;
-                    String num= errorsInt+""; //passem a String
-                    errors.setText(num);
+                    errors.setText(ErrorsEncerts(errorsInt));
                 }
             }
         });
         btnimageButton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(contadorPreguntes==3){
-                    idPregunta++;
-                    preguntaa.setText(preguntesArray.get(idPregunta));
-                    contadorPreguntes++;
-
-
-                    encertsInt++;
-                    String num= encertsInt+""; //passem a String
-                    encerts.setText(num);
-                } else {
-                    errorsInt++;
-                    String num= errorsInt+""; //passem a String
-                    errors.setText(num);
-                }
+                errorsInt++;
+                errors.setText(ErrorsEncerts(errorsInt));
             }
         });
         btnimageButton4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(contadorPreguntes==4){
-                    idPregunta++;
-                    preguntaa.setText(preguntesArray.get(idPregunta));
-                    contadorPreguntes++;
-
-
-                    encertsInt++;
-                    String num= encertsInt+""; //passem a String
-                    encerts.setText(num);
-                } else {
-                    errorsInt++;
-                    String num= errorsInt+""; //passem a String
-                    errors.setText(num);
-                }
+                errorsInt++;
+                errors.setText(ErrorsEncerts(errorsInt));
             }
         });
         btnimageButton5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(contadorPreguntes==5){
-                    idPregunta++;
-                    preguntaa.setText(preguntesArray.get(idPregunta));
-                    contadorPreguntes++;
-
-
-                    encertsInt++;
-                    String num= encertsInt+""; //passem a String
-                    encerts.setText(num);
-                } else {
-                    errorsInt++;
-                    String num= errorsInt+""; //passem a String
-                    errors.setText(num);
-                }
+                errorsInt++;
+                errors.setText(ErrorsEncerts(errorsInt));
             }
         });
         btnimageButton6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(contadorPreguntes==6){
-                    idPregunta++;
-                    contadorPreguntes++;
-
-                    encertsInt++;
-                    String num= encertsInt+""; //passem a String
-                    encerts.setText(num);
-
-                    Intent intent = new Intent( PreguntesJoc1.this, MainActivity.class); //crear intent
-                    startActivity(intent); //executar intent
-                } else {
-                    errorsInt++;
-                    String num= errorsInt+""; //passem a String
-                    errors.setText(num);
-                }
+                errorsInt++;
+                errors.setText(ErrorsEncerts(errorsInt));
             }
         });
+    }
+    public String ErrorsEncerts(int sumar){
+        String num= sumar+""; //passem a String
+        return num;
     }
 }
--- app/src/main/java/com/example/uf1_projecte_final/PreguntesJoc2.java
+++ app/src/main/java/com/example/uf1_projecte_final/PreguntesJoc2.java
@@ -3,14 +3,21 @@ package com.example.uf1_projecte_final;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class PreguntesJoc2 extends AppCompatActivity {
+    Bundle bundle;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.joc2);
+
+        bundle=getIntent().getExtras();
+        int erInt = bundle.getInt("errors");
+        int enInt = bundle.getInt("encerts");
+        //int errorInt, encertsInt;
 
         TextView preguntaa = findViewById(R.id.textViewPregunta);
 
@@ -21,24 +28,35 @@ public class PreguntesJoc2 extends AppCompatActivity {
 
         ImageButton Siguiente = (ImageButton) findViewById(R.id.imatgeButtonNext);
 
-        preguntaa.setText("Quina és la relació entre el disseny de la façana del nou orgue i la ciutat de Valls?");
-        res1.setText("els calçots i els castells");
-        res2.setText("els castells i el campanar");
-        res3.setText("el campanar i els gegants");
-        res4.setText("cap relació");
+        EditText encerts = (EditText) findViewById(R.id.idEncerts);
+        EditText errors = (EditText) findViewById(R.id.idErrors);
+
+        encerts.setText(ErrorsEncerts(enInt));
+        errors.setText(ErrorsEncerts(erInt));
+
+        preguntaa.setText("OBSERVEU BÉ LA IMATGE I RESPONEU");
+        res1.setText("ESTÀ ARRAN DE TERRA");
+        res2.setText("ESTÀ SUSPÉS EN UNA BALCONADA");
+        res3.setText("");
+        res4.setText("");
 
         Siguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (res1.isChecked()) {
-                    preguntaa.setText("Que dia es hoy");
-                    res1.setText("Lunes");
-                    res2.setText("Martes");
-                    res3.setText("Miercoles");
-                    res4.setText("Jueves");
-
+                if (res2.isChecked()) {
+                    encerts.setText(enInt);
+                    preguntaa.setText("QUINA ÉS LA RELACIÓ ENTRE EL DISSENY DE LA FAÇANA DEL NOU ORGUE I LA CIUTAT DE VALLS?");
+                    res1.setText("ELS CALÇOTS I ELS CASTELLS");
+                    res2.setText("ELS CASTELLS I EL CAMPANAR");
+                    res3.setText("EL CAMPANAR I ELS GEGANTS");
+                } else {
+                    //errors.setText(ErrorsEncerts(errorsInt));
                 }
             }
         });
+    }
+    public String ErrorsEncerts(int sumar){
+        String num= sumar+""; //passem a String
+        return num;
     }
 }
