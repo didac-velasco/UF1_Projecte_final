0b4211a8d644ad0acc0e3e6f595db26a170668f0
didac-velasco <Didac2003velasco>
2023-01-25 15:51:14 +0100

    PAG FETA PREGUNTES 2 AMB LA CLASE JAVA

--- app/src/main/java/com/example/uf1_projecte_final/Preguntes2.java
+++ app/src/main/java/com/example/uf1_projecte_final/Preguntes2.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class Preguntes2 extends AppCompatActivity {
     //didac
+    //DIDI
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.joc2);
