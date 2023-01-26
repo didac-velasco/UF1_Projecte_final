12d6ca470d63b8dbb7185e84c70548d4127d1866
didac-velasco <dvelasco@institutjaumehuguet.cat>
2023-01-26 15:19:45 +0100

    NOU LAYOUT ON DINS HI HAN DIFERENTS LAYOUTS PER LES DIFERENTS PREGUNTES. FINS DESPLEGABLES

--- app/src/main/java/com/example/uf1_projecte_final/PreguntesJoc2.java
+++ app/src/main/java/com/example/uf1_projecte_final/PreguntesJoc2.java
@@ -1,16 +1,28 @@
 package com.example.uf1_projecte_final;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class PreguntesJoc2 extends AppCompatActivity {
     Bundle bundle;
     int erInt, enInt;
+
+    LinearLayout layoutradiobutton, layoutrelacio, layoutCaselles;
+
+    String selec;
+
+    Object rel1,rel2,rel3,rel4;
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.joc2);
@@ -18,6 +30,23 @@ public class PreguntesJoc2 extends AppCompatActivity {
         bundle=getIntent().getExtras();
         erInt = bundle.getInt("errors");
         enInt = bundle.getInt("encerts");
+        layoutradiobutton = (LinearLayout) findViewById(R.id.layoutRadioButtons);
+        layoutrelacio = (LinearLayout) findViewById(R.id.layoutrelacionar);
+        layoutCaselles = (LinearLayout) findViewById(R.id.layoutCheckbox);
+
+        //enviar el menú desplegable Android a Java
+        Spinner desp1=findViewById(R.id.spinner1);
+        Spinner desp2=findViewById(R.id.spinner2);
+        Spinner desp3=findViewById(R.id.spinner3);
+        Spinner desp4=findViewById(R.id.spinner4);
+
+        //MOSTRAR ELS ELEMENTS DECLARATS DE STRINGS.XML AL DESPLEGABLE
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.opcions_desple, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        //AQUI DIEM QUIN LAYOUT VOLEM QUE ES VEIGI O NO
+        layoutrelacio.setVisibility(View.INVISIBLE);
+        layoutCaselles.setVisibility(View.INVISIBLE);
 
         TextView preguntaa = findViewById(R.id.textViewPregunta);
 
@@ -51,6 +80,72 @@ public class PreguntesJoc2 extends AppCompatActivity {
                     res1.setText("ELS CALÇOTS I ELS CASTELLS");
                     res2.setText("ELS CASTELLS I EL CAMPANAR");
                     res3.setText("EL CAMPANAR I ELS GEGANTS");
+
+                    if (res2.isChecked()) {
+                        enInt++;
+                        encerts.setText(String.valueOf(enInt));
+
+                        //AQUI DIEM QUIN LAYOUT VOLEM QUE ES VEIGI O NO
+                        layoutradiobutton.setVisibility(View.INVISIBLE);
+                        layoutCaselles.setVisibility(View.INVISIBLE);
+                        layoutrelacio.setVisibility(View.VISIBLE);
+
+                        preguntaa.setText("LLEGIU I RELACIONEU CADA TIPUS D’ORGUE AMB LA DEFINICIÓ QUE CREIEU QUE LI CORRESPON.");
+
+                        desp1.setAdapter(adapter);
+                        //AGAFEM LA POSICIÓ DE L'OPCÓ ESCOLLIDA DEL DESPLEGABLE AMB EL setOnItemSelectedListener
+                        desp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            //S'HA TRIAT UNA OPCIÓ
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                //AGAFEM LA POSICIÓ
+                                rel1 = parent.getItemAtPosition(position);
+                            }
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+
+                        desp2.setAdapter(adapter);
+                        desp2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                rel2 = parent.getItemAtPosition(position);
+                            }
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+                        desp3.setAdapter(adapter);
+                        desp3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                rel3 = parent.getItemAtPosition(position);
+                            }
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+                        desp4.setAdapter(adapter);
+                        desp4.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                rel4 = parent.getItemAtPosition(position);
+                            }
+                            public void onNothingSelected(AdapterView<?> parent) {
+
+                            }
+                        });
+                        //&& desp2.equals(3) && desp3.equals(1) && desp4.equals(4)
+                        /*if(rel1.equals()){
+                            enInt++;
+                            encerts.setText(String.valueOf(enInt));
+
+                            layoutCaselles.setVisibility(View.VISIBLE);
+                            layoutradiobutton.setVisibility(View.INVISIBLE);
+                            layoutrelacio.setVisibility(View.INVISIBLE);
+
+                            preguntaa.setText("MIREU BÉ LA FAÇANA DE L’ORGUE I TRIEU DE TOTES AQUESTES PARTS LES QUE SÓN VISIBLES.");
+
+                        }*/
+                    }
+
                 } else {
                     erInt++;
                     errors.setText(String.valueOf(erInt));
